f02552e2eb6e98d6600f3fd7e62dbcfc8fc8c234
neakiir <filion.jonathan@gmail.com>
2020-12-02 18:39:03 -0500

    AoC 2020 Day2

--- VSRoot/AdventOfCode2020/AdventCalendar/Challenges/Day2Challenge.cs
+++ VSRoot/AdventOfCode2020/AdventCalendar/Challenges/Day2Challenge.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventCalendar.Challenges
@@ -10,100 +11,65 @@ namespace AdventCalendar.Challenges
 	{
 		protected override string Part1()
 		{
-			string entry = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input2.txt");
-			string[] ids = entry.Split('\n');
+			string input = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input2.txt");
+			string[] entries = input.Split('\n');
 
-			int twoCounts = 0;
-			int threeCounts = 0;
-			var characterCounts = new Dictionary<char, int>();
-
-			foreach(var id in ids)
+			int totalSuccess = 0;
+			string[] delimiters = new string[] { "-",": "," ","\r"};
+			foreach(var entry in entries)
 			{
-				characterCounts.Clear();
+				string[] result = entry.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
 
-				foreach (var c in id)
-				{
-					if(!characterCounts.ContainsKey(c))
+				int minCount = int.Parse(result[0]);
+				int maxCount = int.Parse(result[1]);
+
+				char letter = result[2].ToCharArray()[0];
+				string password = result[3];
+
+				int count = 0;
+				foreach(var pass in password)
+				{ 
+					if(pass == letter)
 					{
-						characterCounts.Add(c,0);
+						count++;
 					}
-					characterCounts[c]++;
 				}
 
-				bool has2Count = false;
-				bool has3Count = false;
-				foreach(var charCount in characterCounts)
+				if(count >= minCount && count <= maxCount)
 				{
-					if(!has2Count && charCount.Value == 2)
-					{
-						twoCounts++;
-						has2Count = true;
-						if (has3Count)
-						{
-							break;
-						}
-					}
-					else if(!has3Count && charCount.Value == 3)
-					{
-						threeCounts++;
-						has3Count = true;
-						if (has2Count)
-						{
-							break;
-						}
-					}
+					totalSuccess++;
 				}
 			}
+			
 
-			return (twoCounts * threeCounts).ToString();
+			return totalSuccess.ToString();
 		}
 
 		protected override string Part2()
 		{
-			string entry = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input2.txt");
-			string[] ids = entry.Split('\n');
+			string input = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input2.txt");
+			string[] entries = input.Split('\n');
 
-			// find the ids with 1 difference, return the same part.
-			for(int i = 0;i<ids.Length;++i)
+			int totalSuccess = 0;
+			string[] delimiters = new string[] { "-", ": ", " ", "\r" };
+			foreach (var entry in entries)
 			{
-				for(int j = i;j < ids.Length;++j)
+				string[] result = entry.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+				int firstPos = int.Parse(result[0]);
+				int secondPos = int.Parse(result[1]);
+
+				char letter = result[2].ToCharArray()[0];
+				char[] password = result[3].ToCharArray();
+
+				if (password[firstPos - 1] == letter ^ password[secondPos - 1] == letter)
 				{
-					int idx = CompareIds(ids[i],ids[j]);
-					if(idx != -1)
-					{
-						return ids[i].Remove(idx, 1);
-					}
+					totalSuccess++;
 				}
 			}
 
-			return "NOT FOUND";
-		}
 
-		/// <summary>
-		/// Returns -1 if there are not 1 differences. Returns the index of the difference if a single one is found.
-		/// </summary>
-		/// <param name="id1"></param>
-		/// <param name="id2"></param>
-		/// <returns></returns>
-		private int CompareIds(string id1, string id2)
-		{
-			int index = -1;
-			int differences = 0;
-			for(int i = 0;i<id1.Length;++i)
-			{
-				if(id1[i] != id2[i])
-				{
-					index = i;
-					differences++;
-
-					if(differences > 1)
-					{
-						return -1;
-					}
-				}
-			}
-			
-			return index;
+			return totalSuccess.ToString();
 		}
 	}
 }
