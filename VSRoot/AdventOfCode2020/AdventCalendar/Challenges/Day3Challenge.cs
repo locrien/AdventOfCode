27346ba6617b9213b977f588025ae577e16c31cd
neakiir <filion.jonathan@gmail.com>
2020-12-03 23:40:54 -0500

    AoC 2020 Day 3

--- VSRoot/AdventOfCode2020/AdventCalendar/Challenges/Day3Challenge.cs
+++ VSRoot/AdventOfCode2020/AdventCalendar/Challenges/Day3Challenge.cs
@@ -9,125 +9,60 @@ namespace AdventCalendar.Challenges
 {
 	class Day3Challenge : Challenge
 	{
-		private struct Position
-		{
-			public int X;
-			public int Y;
-
-			public Position(int x, int y)
-			{
-				X = x;
-				Y = y;
-			}
-		}
-
 		protected override string Part1()
 		{
-			string entry = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input3.txt");
-			string[] ids = entry.Split('\n');
+			string input = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input3.txt");
 
-			var claimedSpots = new Dictionary<Position, int>();
+			var repeatWidth = input.IndexOf('\r');
+			var positions = input.ToCharArray();
 
-			// parse the data
-			// format : #1 @ 916,616: 21x29
+			var collisionCount = CalculateCollisions(3,1, positions, repeatWidth);
 
-			// find the overlap between all the entries
-			foreach(var id in ids)
-			{
-				int identifierIdx = id.IndexOf('#', 0);
-				int positionIdx = id.IndexOf('@', identifierIdx + 1);
-				int posSepIdx = id.IndexOf(',', positionIdx + 1);
-				int areaIdx = id.IndexOf(':', posSepIdx + 1);
-				int areaSepIdx = id.IndexOf('x', areaIdx + 1);
-
-				int identifier = int.Parse(id.Substring(identifierIdx + 1, positionIdx - identifierIdx - 1));
-				int posX = int.Parse(id.Substring(positionIdx + 1, posSepIdx - positionIdx - 1));
-				int posY = int.Parse(id.Substring(posSepIdx + 1, areaIdx - posSepIdx - 1));
-				int areaX = int.Parse(id.Substring(areaIdx + 1, areaSepIdx - areaIdx - 1));
-				int areaY = int.Parse(id.Substring(areaSepIdx+1));
-
-				for(int i = posX; i < posX + areaX;++i)
-				{
-					for(int j = posY;j<posY+areaY;++j)
-					{
-						var claimedPosition = new Position(i,j);
-						if(!claimedSpots.ContainsKey(claimedPosition))
-						{
-							claimedSpots.Add(claimedPosition,1);
-						}
-						else
-						{
-							claimedSpots[claimedPosition]++;
-						}
-					}
-				}
-			}
-
-			int overlapSpots = 0;
-			foreach(var claimedSpot in claimedSpots)
-			{
-				if(claimedSpot.Value > 1)
-				{
-					overlapSpots++ ;
-				}
-			}
-
-			return overlapSpots.ToString();
+			return collisionCount.ToString();
 		}
 
-		protected override string Part2()
+		private int CalculateCollisions(int slopeX, int slopeY, char[] positions, int repeatWidth)
 		{
-			string entry = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input3.txt");
-			string[] ids = entry.Split('\n');
+			var x = 0;
+			var y = 0;
+			
+			var index = 0;
 
-			var claimedSpots = new Dictionary<Position, List<int>>();
+			var collisionCount = 0;
 
-			// parse the data
-			// format : #1 @ 916,616: 21x29
-			var claims = new List<int>();
-
-			// find the overlap between all the entries
-			foreach (var id in ids)
+			while (index < positions.Length - 1)
 			{
-				int identifierIdx = id.IndexOf('#', 0);
-				int positionIdx = id.IndexOf('@', identifierIdx + 1);
-				int posSepIdx = id.IndexOf(',', positionIdx + 1);
-				int areaIdx = id.IndexOf(':', posSepIdx + 1);
-				int areaSepIdx = id.IndexOf('x', areaIdx + 1);
-
-				int identifier = int.Parse(id.Substring(identifierIdx + 1, positionIdx - identifierIdx - 1));
-				int posX = int.Parse(id.Substring(positionIdx + 1, posSepIdx - positionIdx - 1));
-				int posY = int.Parse(id.Substring(posSepIdx + 1, areaIdx - posSepIdx - 1));
-				int areaX = int.Parse(id.Substring(areaIdx + 1, areaSepIdx - areaIdx - 1));
-				int areaY = int.Parse(id.Substring(areaSepIdx + 1));
-				claims.Add(identifier);
-
-				for (int i = posX; i < posX + areaX; ++i)
+				var element = positions[index];
+				switch (element)
 				{
-					for (int j = posY; j < posY + areaY; ++j)
-					{
-						var claimedPosition = new Position(i, j);
-						if (!claimedSpots.ContainsKey(claimedPosition))
-						{
-							var claimantsList = new List<int>();
-							claimantsList.Add(identifier);
-							claimedSpots.Add(claimedPosition, claimantsList);
-						}
-						else
-						{
-							claimedSpots[claimedPosition].Add(identifier);
-							foreach(var claimant in claimedSpots[claimedPosition])
-							{
-								claims.Remove(claimant);
-							}
-						}
-					}
+					case '#':
+						collisionCount++;
+						break;
 				}
+
+				x += slopeX;
+				y += slopeY;
+
+				index = (x % (repeatWidth)) + (y * (repeatWidth + 2));
 			}
 
+			return collisionCount;
+		}
+		
+		protected override string Part2()
+		{
+			string input = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input3.txt");
 
+			var repeatWidth = input.IndexOf('\r');
+			var positions = input.ToCharArray();
 
-			return claims[0].ToString() ;
+			long count = CalculateCollisions(1, 1, positions, repeatWidth);
+			count *= CalculateCollisions(3, 1, positions, repeatWidth);
+			count *= CalculateCollisions(5, 1, positions, repeatWidth);
+			count *= CalculateCollisions(7, 1, positions, repeatWidth);
+			count *= CalculateCollisions(1, 2, positions, repeatWidth);
+			
+			return count.ToString();
 		}
 	}
 }
