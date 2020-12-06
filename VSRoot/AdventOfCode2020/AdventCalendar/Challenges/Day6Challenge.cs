6e607c4eb35efb2632f5cb548d5e52bc79fc6fb7
neakiir <filion.jonathan@gmail.com>
2020-12-06 00:18:27 -0500

    2020 Day 6

--- VSRoot/AdventOfCode2020/AdventCalendar/Challenges/Day6Challenge.cs
+++ VSRoot/AdventOfCode2020/AdventCalendar/Challenges/Day6Challenge.cs
@@ -11,15 +11,68 @@ namespace AdventCalendar.Challenges
 	{
 		protected override string Part1()
 		{
-			string entry = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input3.txt");
-			string[] ids = entry.Split('\n');
-			
-			return "";
+			string input = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input6.txt");
+			string[] groups = input.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+			int questionSum = 0;
+			var questions = new HashSet<char>();
+			foreach(var group in groups)
+			{
+				questions.Clear();
+				string[] people = group.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+				foreach(var person in people)
+				{
+					foreach(var question in person)
+					{
+						if(!questions.Contains(question))
+						{
+							questions.Add(question);
+						}
+					}
+				}
+
+				questionSum += questions.Count;
+			}
+
+			return questionSum.ToString();
 		}
 
 		protected override string Part2()
 		{
-			return "";
+			string input = Utils.ReadEmbededResourceTextFile("AdventCalendar.Data.input6.txt");
+			string[] groups = input.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+			int questionSum = 0;
+			var questions = new Dictionary<char,int>();
+			foreach (var group in groups)
+			{
+				questions.Clear();
+				string[] people = group.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+				foreach (var person in people)
+				{
+					foreach (var question in person)
+					{
+						if (!questions.ContainsKey(question))
+						{
+							questions.Add(question,1);
+						}
+						else
+						{
+							questions[question]++;
+						}
+					}
+				}
+
+				foreach (var question in questions.Keys)
+				{
+					if(questions[question] == people.Length)
+					{
+						questionSum ++;
+					}
+				}
+			}
+
+			return questionSum.ToString();
 		}
 	}
 }
